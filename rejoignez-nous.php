1e37aa720102ad38a0aad364fcfb8bfe0037f482
Ibrahim CISSE <ibraci1960@gmail.com>
2017-02-16 23:35:05 +0000

    Suppression du dossier mockups et Ajout de la page rejoignez-nous.php

---
+++ rejoignez-nous.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="description" content="The Page Description">
+		<style type="text/css">@-ms-viewport{width: device-width;}</style>
+		<title>Rejoingnez-nous | APJEC</title>
+		<link rel="stylesheet" href="css/layers.min.css" media="screen">
+		<link rel="stylesheet" href="css/font-awesome.min.css" media="screen"> 
+		<link rel="stylesheet" href="css/style.css" media="screen">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+		<link rel="icon" type="image/png" href="favicon.ico" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">		
+	</head>
+
+	<body class="page">
+
+		<header role="banner" class="transparent light">
+			<div class="row">
+				<div class="nav-inner row-content buffer-left buffer-right even clear-after">
+					<div id="brand">
+						<h1 class="reset"><!--<img src="img/logo.png" alt="logo">--><a href="index.html">Semako</a></h1>
+					</div><!-- brand -->
+					<a id="menu-toggle" href="#"><i class="fa fa-bars fa-lg"></i></a>
+					<nav>
+						<ul class="reset" role="navigation">
+							<li class="menu-item"><a href="index.php">Accueil</a></li>
+							<li class="menu-item"><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+							<li class="menu-item"><a href="notre-journal.php">Notre Journal</a></li>
+							<li class="menu-item"><a href="contact.php">Contact</a></li>
+						</ul>
+					</nav>
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</header>
+
+		<main role="main">
+			<div id="intro-wrap" data-height="22.222">
+				<div id="intro" class="preload darken" data-autoplay="5000" data-navigation="true" data-pagination="true" data-transition="fade">
+					<div class="intro-item" style="background-image: url(img/06.jpg);">
+					</div>
+					<div class="intro-item" style="background-image: url(img/04.jpg);">
+					</div>	
+					<div class="intro-item" style="background-image: url(img/05.jpg);">
+					</div>														
+				</div><!-- intro -->				
+			</div><!-- intro-wrap -->
+
+			<div id="main" class="row">
+				<div class="row-content buffer-left buffer-right buffer-bottom clear-after">
+					
+					<div class="post-area clear-after">
+						<article role="main" class="column nine">
+							<h2>Comment adhérer à l’association ?</h2>
+							<p>L’adhésion à l’association est volontaire : Sont membres actifs de l’association, toutes personnes âgées  au minimum de 12ans et au Maximum de 35ans et  s’acquittant de leur Droit d’adhésion, de leur cotisation et  participe à la suite à la réalisation des activités de l’association. Les adhérents disposent des droits égaux dans l’administration et dans la gestion de l’association. Il ne peut être  établi entre eux aucune discrimination notamment la différence de Sexe, de position sociale, d’appartenance religieuse, politique ou la date de leur adhésion.</p>
+							<!-- <div class="gallery slider" data-autoplay="false" data-autoheight="true">
+								<figure>
+									<div><img src="http://placehold.it/800x600/ddd/fff&text=Beetle%20image" alt=""></div>
+									<div><img src="http://placehold.it/800x600/ddd/fff&text=Beetle%20image" alt=""></div>	
+									<div><img src="http://placehold.it/800x600/ddd/fff&text=Beetle%20image" alt=""></div>
+								</figure>
+							</div> --><!-- gallery -->						
+						</article>	
+
+					</div><!-- post-area -->
+
+				</div><!-- row-content -->
+			</div><!-- row -->			
+		</main><!-- main -->
+
+		<footer role="contentinfo">
+			<div class="row">
+				<div class="row-content buffer clear-after">
+					<section id="top-footer">
+						<div class="widget column three"><!-- la class="widget" è forse generata utomaticamente da wp -->
+							<h4>Menu</h4>
+							<ul class="plain">
+								<li><a href="index.php">Accueil</a></li>
+								<li><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+								<li><a href="notre-journal.php">Notre Journal</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</div>
+						<div class="widget column three">
+							<h4>Autres</h4>
+							<ul class="plain">
+								<li><a href="rejoignez-nous.php">Rejoignez-nous</a></li>
+								<li><a href="#">Nos Activites</a></li>
+								<li><a href="#">Nos Realisations</a></li>
+								<li><a href="#">Domaine d'intervention</a></li>
+								<li><a href="#">Galerie</a></li>
+							</ul>
+						</div>								
+						<div class="widget column three">
+							<h4>Adresse</h4>
+							<p>
+								+223 66 00 80 08<br>
+								apjec@apjec.ml<br>
+								Cite des Enfants,<br>
+								Niamakoro,<br>
+								Bamako,<br>
+								Mali.
+							</p>
+						</div>				
+						<div class="widget meta-social column three">
+							<h4>Suivez-nous</h4>
+							<ul class="inline">
+								<li><a href="https://twitter.com/ongapjec" class="twitter-share border-box"><i class="fa fa-twitter fa-lg"></i></a></li>
+								<li><a href="https://www.facebook.com/ongapjec/" class="facebook-share border-box"><i class="fa fa-facebook fa-lg"></i></a></li>
+								<li><a href="#" class="youtube-share border-box"><i class="fa fa-youtube fa-lg"></i></a></li>
+							</ul>
+						</div>														
+					</section><!-- top-footer -->
+					<section id="bottom-footer">
+						<p class="keep-left">&copy; 2017 <a href="http://apjec.ml" alt="APJEC">APJEC</a>. Powered by <a href="https://ibracilinks.ml" alt="Ibraci Links">Ibraci Links</a></p>
+					</section><!-- bottom-footer -->			
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</footer>
+
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>		
+		<script src="js/plugins.js"></script>
+		<script src="js/beetle.js"></script>
+
+	</body>
+
+</html>
