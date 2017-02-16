8ffd37a20c5435ed19eed094cadc7e532078f9bb
Ibrahim CISSE <ibraci1960@gmail.com>
2017-02-16 17:24:16 +0000

    Envoie de tous les dossiers dur git

---
+++ notre-journal.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="description" content="The Page Description">
+		<style type="text/css">@-ms-viewport{width: device-width;}</style>
+		<title>Notre Journal | APJEC</title>
+		<link rel="stylesheet" href="css/layers.min.css" media="screen">
+		<link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+		<link rel="stylesheet" href="css/style.css" media="screen">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+		<link rel="icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">		
+	</head>
+
+	<body class="page">
+
+		<header role="banner" class="transparent light">
+			<div class="row">
+				<div class="nav-inner row-content buffer-left buffer-right even clear-after">
+					<div id="brand">
+						<h1 class="reset"><a href="index.html">APJEC</a></h1>
+					</div><!-- brand -->
+					<a id="menu-toggle" href="#"><i class="fa fa-bars fa-lg"></i></a>
+					<nav>
+						<ul class="reset" role="navigation">
+							<li class="menu-item"><a href="index.php">Accueil</a></li>
+							<li class="menu-item"><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+							<li class="menu-item"><a href="notre-journal.php">Notre Journal</a></li>
+							<li class="menu-item"><a href="contact.php">Contact</a></li>
+						</ul>
+					</nav>
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</header>
+
+		<main role="main">
+			<div id="intro-wrap"  data-height="22.222">
+				<div id="intro" class="preload darken">
+					<div class="intro-item" style="background-image: url(img/06.jpg);">
+						<div class="caption">
+							<h2>Notre Journal, le Regards des Enfants</h2>
+						</div><!-- caption -->		
+					</div><!-- intro -->
+				</div><!-- intro -->				
+			</div><!-- intro-wrap -->
+
+			<div id="main" class="row">
+				<div class="row-content buffer-left buffer-right buffer-bottom clear-after">
+					<div class="column nine">
+						<article class="clear-after">
+							<div class="column three">
+								<figure><img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt=""></figure>
+							</div><!-- column three -->
+							<div class="column nine last">
+								<h2><a href="#">SEBA 2016</a></h2>
+								<h5 class="meta-post"><time datetime="2013-11-10">10 November 2016</time></h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>	
+							</div><!-- column nine -->
+						</article>
+						<article class="clear-after">
+							<div class="column three">
+								<figure><img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt=""></figure>
+							</div><!-- column three -->
+							<div class="column nine last">
+								<h2><a href="#">Semaine Nationale</a></h2>
+								<h5 class="meta-post"><time datetime="2013-11-10">10 November 2016</time></h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>	
+							</div><!-- column nine -->
+						</article>
+						<article class="clear-after">
+							<div class="column three">
+								<figure><img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt=""></figure>
+							</div><!-- column three -->
+							<div class="column nine last">
+								<h2><a href="#">Oxy-Jeunes</a></h2>
+								<h5 class="meta-post"><time datetime="2013-11-10">10 November 2016</time></h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>	
+							</div><!-- column nine -->
+						</article>
+						<article class="clear-after">
+							<div class="column three">
+								<figure><img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt=""></figure>
+							</div><!-- column three -->
+							<div class="column nine last">
+								<h2><a href="#">Maxi jeunes</a></h2>
+								<h5 class="meta-post"><time datetime="2013-11-10">10 November 2016</time></h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>	
+							</div><!-- column nine -->
+						</article>	
+						<div id="pagination">
+							<ul class="clear-after reset plain">
+								<li id="older" class="pagination-nav"><a href="#" class="button transparent aqua"><i class="fa fa-chevron-left"></i><span class="label">Precedent</span></a></li> 
+								<li id="newer" class="pagination-nav"><a href="#" class="button transparent aqua"><span class="label">Suivant</span><i class="fa fa-chevron-right"></i></a></li>  
+							</ul>
+						</div>											
+					</div><!-- column nine -->
+				</div><!-- row-content -->
+			</div><!-- row -->
+		</main><!-- main -->
+
+		<footer role="contentinfo">
+			<div class="row">
+				<div class="row-content buffer clear-after">
+					<section id="top-footer">
+						<div class="widget column three"><!-- la class="widget" è forse generata utomaticamente da wp -->
+							<h4>Menu</h4>
+							<ul class="plain">
+								<li><a href="index.php">Accueil</a></li>
+								<li><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+								<li><a href="notre-journal.php">Notre Journal</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</div>
+						<div class="widget column three">
+							<h4>Autres</h4>
+							<ul class="plain">
+								<li><a href="rejoignez-nous.php">Rejoignez-nous</a></li>
+								<li><a href="#">Nos Activites</a></li>
+								<li><a href="#">Nos Realisations</a></li>
+								<li><a href="#">Domaine d'intervention</a></li>
+								<li><a href="#">Galerie</a></li>
+							</ul>
+						</div>								
+						<div class="widget column three">
+							<h4>Adresse</h4>
+							<p>
+								+223 66 00 80 08<br>
+								apjec@apjec.ml<br>
+								Niamakoro,<br>
+								Cite des Enfants,<br>
+								Bamako,<br>
+								Mali.
+							</p>
+						</div>				
+						<div class="widget meta-social column three">
+							<h4>Follow Us</h4>
+							<ul class="inline">
+								<li><a href="#" class="twitter-share border-box"><i class="fa fa-twitter fa-lg"></i></a></li>
+								<li><a href="#" class="facebook-share border-box"><i class="fa fa-facebook fa-lg"></i></a></li>
+								<li><a href="#" class="pinterest-share border-box"><i class="fa fa-pinterest fa-lg"></i></a></li>
+							</ul>
+						</div>														
+					</section><!-- top-footer -->
+					<section id="bottom-footer">
+						<p class="keep-left">&copy; 2017 <a href="http://apjec.ml" alt="APJEC">APJEC</a>. Powered by <a href="https://ibracilinks.ml" alt="Ibraci Links">Ibraci Links</a></p>
+					</section><!-- bottom-footer -->			
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</footer>
+
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>		
+		<script src="js/plugins.js"></script>
+		<script src="js/beetle.js"></script>
+
+	</body>
+
+</html>
---
+++ qui-sommes-nous.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="description" content="The Page Description">
+		<style type="text/css">@-ms-viewport{width: device-width;}</style>
+		<title>APJEC | Action pour la Promotion des Jeunes et Enfants Communicateurs</title>
+		<link rel="stylesheet" href="css/layers.min.css" media="screen">
+		<link rel="stylesheet" href="css/font-awesome.min.css" media="screen"> 
+		<link rel="stylesheet" href="css/style.css" media="screen">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+
+
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+		<link rel="icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">		
+	</head>
+
+	<body class="page">
+
+		<header role="banner" class="transparent light">
+			<div class="row">
+				<div class="nav-inner row-content buffer-left buffer-right even clear-after">
+					<div id="brand">
+						<h1 class="reset"><a href="index.html">APJEC</a></h1>
+					</div><!-- brand -->
+					<a id="menu-toggle" href="#"><i class="fa fa-bars fa-lg"></i></a>
+					<nav>
+						<ul class="reset" role="navigation">
+							<li class="menu-item"><a href="index.php">Accueil</a></li>
+							<li class="menu-item"><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+							<li class="menu-item"><a href="notre-journal.php">Notre Journal</a></li>
+							<li class="menu-item"><a href="contact.php">Contact</a></li>
+						</ul>
+					</nav>
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</header>
+
+		<main role="main">
+			<div id="intro-wrap">
+				<div id="intro" class="preload" data-autoplay="5000" data-navigation="true" data-pagination="true" data-transition="fade">					
+					<div class="intro-item" style="background-color: #70767C;">		
+						<div class="intro-mockup-wrapper">		
+							<div class="caption-mockup caption-right column six last-special">
+								<h2>Ensemble, Construisons un monde meilleur pour les enfants</h2>
+							</div><!-- caption -->
+							<div class="intro-mockup intro-left column six">
+								<img src="img/02.jpg" alt="">
+							</div><!-- intro-mockup -->							
+						</div><!-- intro-mockup-wrapper -->
+					</div>					
+					<div class="intro-item" style="background-color: #f5be55;">
+						<div class="intro-mockup-wrapper">						
+							<div class="caption-mockup">
+								<h2>Engagez-vous, faites-vous entendre.</h2>
+							</div><!-- caption -->	
+																				
+							<div class="intro-mockup">
+								<img src="img/03.jpg" alt="">
+							</div><!-- intro-mockup -->	
+						</div><!-- intro-mockup-wrapper -->										
+					</div>
+					<div class="intro-item" style="background-color: #6b3f4f;">
+						<div class="intro-mockup-wrapper">
+							<div class="caption-mockup caption-left column six">
+								<h2>Les enfants ont besoin de défenseurs.</h2>
+								<a class="button white transparent" href="rejoingnez-nous.php">Rejoingnez-nous</a>
+							</div><!-- caption -->												
+							<div class="intro-mockup intro-right column six last">
+								<img src="img/04.jpg" alt="">
+							</div><!-- intro-mockup -->	
+						</div><!-- intro-mockup-wrapper -->
+					</div>	
+					<div class="intro-item" style="background-color: #32cba3;">
+						<div class="intro-mockup-wrapper">						
+							<div class="caption-mockup">
+								<h2>Regards des Enfants</h2>
+							</div><!-- caption -->													
+							<div class="intro-mockup">
+								<img src="img/05.jpg" alt="">
+							</div><!-- intro-mockup -->	
+						</div><!-- intro-mockup-wrapper -->										
+					</div>
+				</div><!-- intro -->
+			</div><!-- intro-wrap -->
+
+			<div id="main">
+
+				<div class="post-area clear-after about">
+					<article role="main">
+						<h1>Qui Sommes-Nous ?</h1>
+						<p>APJEC, Action pour la Promotion des Jeunes et Enfants Communicateurs, est une association apolitique à but non lucratif et humanitaire, intervenant dans toutes les régions et du district de Bamako dans le cadre de la promotion des droits et devoirs des enfants.</p>
+						<p>Notre but est de créer un cadre de concertation et d’échanges d’expériences entre les enfants intervenant dans les médias ou d’autres formes d’expressions pour la promotion et la protection de leurs droits et devoirs.</p>
+						<img src="img/apjec520.jpg" style="margin: 0 auto;">
+						<p>Le but de l’Action pour la Promotion des Jeunes et Enfants Communicateurs est de créer un cadre de concertation ; d’échanges et de partage d’expériences entre les enfants intervenant dans les média ou d’autres formes d’expressions pour la promotion et la protection de leurs droits et devoirs. Regroupant filles et garçons, l’association est à but humanitaire et non lucratif. </p>
+						<iframe width="285" height="325" src="https://www.youtube.com/embed/AXw8Hk9FdhQ" frameborder="0" allowfullscreen></iframe>
+						<p>C’est ainsi lors de sa 1er Assemblée Générale ordinaire tenue en décembre 2006, un Plan d’Action a été élaboré conformément aux trois sous commissions.</p>
+					</article>
+
+				</div><!-- post-area -->				
+
+			</div><!-- id-main -->
+		</main><!-- main -->
+
+		<footer role="contentinfo">
+			<div class="row">
+				<div class="row-content buffer clear-after">
+					<section id="top-footer">
+						<div class="widget column three"><!-- la class="widget" è forse generata utomaticamente da wp -->
+							<h4>Menu</h4>
+							<ul class="plain">
+								<li><a href="index.php">Accueil</a></li>
+								<li><a href="qui-sommes-nous.php">Qui sommes-nous ?</a></li>
+								<li><a href="notre-journal.php">Notre Journal</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</div>
+						<div class="widget column three">
+							<h4>Autres</h4>
+							<ul class="plain">
+								<li><a href="rejoignez-nous.php">Rejoignez-nous</a></li>
+								<li><a href="#">Nos Activites</a></li>
+								<li><a href="#">Nos Realisations</a></li>
+								<li><a href="#">Domaine d'intervention</a></li>
+								<li><a href="#">Galerie</a></li>
+							</ul>
+						</div>								
+						<div class="widget column three">
+							<h4>Adresse</h4>
+							<p>
+								+223 66 00 80 08<br>
+								apjec@apjec.ml<br>
+								Niamakoro,<br>
+								Cite des Enfants,<br>
+								Bamako,<br>
+								Mali.
+							</p>
+						</div>				
+						<div class="widget meta-social column three">
+							<h4>Suivez-nous</h4>
+							<ul class="inline">
+								<li><a href="#" class="twitter-share border-box"><i class="fa fa-twitter fa-lg"></i></a></li>
+								<li><a href="https://www.facebook.com/ongapjec/" class="facebook-share border-box"><i class="fa fa-facebook fa-lg"></i></a></li>
+								<li><a href="#" class="pinterest-share border-box"><i class="fa fa-pinterest fa-lg"></i></a></li>
+							</ul>
+						</div>														
+					</section><!-- top-footer -->
+					<section id="bottom-footer">
+						<p class="keep-left">&copy; 2017 <a href="http://apjec.ml" alt="APJEC">APJEC</a>. Powered by <a href="https://ibracilinks.ml" alt="Ibraci Links">Ibraci Links</a></p>
+					</section><!-- bottom-footer -->			
+				</div><!-- row-content -->	
+			</div><!-- row -->	
+		</footer>
+
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>		
+		<script src="js/plugins.js"></script>
+		<script src="js/beetle.js"></script>
+
+	</body>
+
+</html>
